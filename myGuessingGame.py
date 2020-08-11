37e64f86cbcb7a9f19da4c2349db0d0797f87c3a
nealchristopherb <christopherbneal@gmail.com>
2020-08-10 23:27:21 -0400

    number of tries return works

--- myGuessingGame.py
+++ myGuessingGame.py
@@ -2,8 +2,8 @@ import random
 # Generate random number between 1 and 20
 secretNum=random.randint(1,20)
 print (secretNum)
-# Initialize number of guesses to zero
-number_of_guesses= 0 
+# Initialize number of guesses to one
+number_of_guesses= 1 
 # User interaction, including input and guess prompt
 print ("What is your name?")
 userName= input()
@@ -12,12 +12,13 @@ print ("Hello " + userName + " I have picked a random number between 1 and 20.  
 while number_of_guesses <5:
     userGuess=int(input())    
     if secretNum < userGuess:
-        print ("Your guess is too high. Try again")
+        print ("Your guess is too high. Try again." )
         number_of_guesses=number_of_guesses+1
     if secretNum > userGuess:
         print ("Your guess is too low. Try again")
         number_of_guesses=number_of_guesses+1    
     if userGuess==secretNum:
-        print("Congratulations! You got it!")
+        number_of_guesses= str(number_of_guesses)
+        print ("Congratulations! You guessed it in " + number_of_guesses + " try!")
         break   
         
