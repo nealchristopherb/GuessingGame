1a32fea5d2574fc65bfc910b75fe4e4dd693abc5
nealchristopherb <christopherbneal@gmail.com>
2020-08-11 19:15:12 -0400

    Added line for compatability with Python v2

--- myGuessingGame.py
+++ myGuessingGame.py
@@ -1,12 +1,15 @@
 import random
 # Generate random number between 1 and 20
 secretNum=random.randint(1,20)
+# Display secretNum to show that project works correctly.  Would be removed otherwise.
 print (secretNum)
 # Initialize number of guesses to zero
 number_of_guesses= 0 
 # User interaction, including input and guess prompt
 print ("What is your name?")
 userName= input()
+# User name prompt using raw_input for v2:
+# userName = raw_input("What is your name? ")
 print ("Hello, " + userName + ", I have picked a random number between 1 and 20.  Lets see if you can guess it" )
 # While loop cycles user guesses and prompts user to try again if their guess is higher or lower than secretNum.
 while number_of_guesses <5:
@@ -15,13 +18,14 @@ while number_of_guesses <5:
         if userGuess > 20:
             print ("I said keep it between 1 and 20.   Don't make me come over there!")  
         else: print ("Your guess is too high. Try again." )
-#  number_of_guesses is ncreased on failed attempts       
+#  number_of_guesses is 1ncreased on failed attempts       
         number_of_guesses=number_of_guesses+1
     if secretNum > userGuess:
         if userGuess < 1:
             print ("I said keep it between 1 and 20.  Don't make me come over there!") 
         else: print ("Your guess is too low. Try again")
         number_of_guesses=number_of_guesses+1 
+           
 # If user guesses correctly:            
     if userGuess==secretNum:
         totalGuesses= str(number_of_guesses +1)
@@ -34,5 +38,3 @@ while number_of_guesses <5:
 if number_of_guesses >4:
     correctNum= str(secretNum)
     print ("I'm sorry, " + userName + ", but you failed to guess the number in 5 tries.  The correct number was " + correctNum + ".")
-# if userGuess > 20:
-#     print ("Please choose a number between 1 and 20")   
