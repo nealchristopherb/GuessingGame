26eb126275102e6538fd79bd79a2f0409f2d10f2
nealchristopherb <christopherbneal@gmail.com>
2020-08-10 22:20:44 -0400

    more or less works but return line is infinite

--- myGuessingGame.py
+++ myGuessingGame.py
@@ -1,1 +1,22 @@
-in
+import random
+# Generate random number between 1 and 20
+secretNum=random.randint(1,20)
+print (secretNum)
+# Initialize number of guesses to zero
+number_of_guesses= 0 
+# User interaction, including input and guess prompt
+print ("What is your name?")
+userName= input()
+print ("Hello " + userName + " I have picked a random number between 1 and 20.  Lets see if you can guess it" )
+userGuess=int(input())
+while number_of_guesses <5:
+    if secretNum < userGuess:
+        print ("Your guess is too high. Try again")
+        number_of_guesses=number_of_guesses+1
+        if secretNum > userGuess:
+            print ("Your guess is too low. Try again")
+        number_of_guesses=number_of_guesses+1    
+    if userGuess==secretNum:
+        print("Congratulations! You got it!")
+           
+        
